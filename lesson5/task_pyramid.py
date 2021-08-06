8e5947856163d4ff0d397c062c3c3636034d0755
Sergey Yaschenko <saysevod@gmail.com>
2021-08-06 09:17:10 +0300

    lesson5-2

--- lesson5/task_pyramid.py
+++ lesson5/task_pyramid.py
@@ -3,14 +3,18 @@ MAX = 9
 
 
 def main():
-    number = int(input(f'Input digit from  {MIN} to {MAX}, please: '))
-    if number in range(MIN, MAX + 1):
-        for i in range(1, number + 1):
-            str_row = ''.join(list(map(str, range(1, i+1))))
-            str_row += ''.join(list(map(str, range(i-1, 0, -1))))
-            print(str_row)
+    try:
+        number = int(input(f'Input digit from  {MIN} to {MAX}, please: '))
+    except ValueError:
+        print('Your number is not valid.')
     else:
-        print(f'{number} not in range {MIN}..{MAX}')
+        if number in range(MIN, MAX + 1):
+            for i in range(1, number + 1):
+                str_row = ''.join(list(map(str, range(1, i+1))))
+                str_row += ''.join(list(map(str, range(i-1, 0, -1))))
+                print(str_row)
+        else:
+            print(f'{number} not in range {MIN}..{MAX}')
 
 
 if __name__ == '__main__':
