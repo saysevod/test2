58822e21c284e2355c7220dfefa456b2254320a0
Sergey Yaschenko <saysevod@gmail.com>
2021-08-09 09:58:40 +0300

    lesson6-2

--- lesson6/task_unique.py
+++ lesson6/task_unique.py
@@ -3,7 +3,7 @@ def main():
     list_second = input(f'Input second list, please(ex. 0 5 5 12): ').split()
     # TODO list_first, list_second validate to fill
     print(
-        f'List of unique value from both lists {len([x for x in list_first if list_first.count(x) == list_second.count(x) == 1])}')
+        f'List of unique value from lists {len([x for x in list_first + list_second if (list_first + list_second).count(x) == 1])}')
 
 
 if __name__ == '__main__':
